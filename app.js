74388b7e90f94a6cf2d5ba945df675e3c21ec38d
Hector Gonzalez <raftb.gonzalez@outlook.com>
2018-10-23 23:07:09 +0000

    Close

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const express = require('express');
+var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
 
-app.get
-app.use
-app.post
-app.delete
+// Displays name in browser
+app.get('/', function(req, res){
+    res.send('Hector Gonzalez');
+})
+
+// Request any name in the browser using a dynamic get route
+app.get('/user/:name', function(req, res){
+    res.send('Hey ' + req.params.name + '!');
+})
+
+app.get('/test/add', (req, res) => { // to use: /testing/adding?num1=x&num2=y  
+    const num1 = parseInt(req.query.num1, 10);
+    const num2 = parseInt(req.query.num2, 10);
+    res.send(`<h1>Hey you wanna add: ${num1} plus ${num2} is now ${num1 + num2}</h1>`);
+    
+});
+
+app.listen(process.env.PORT, process.env.IP, 8080, function(){
+    console.log('Listening to port 8080')
+})
